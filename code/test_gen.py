c574f112d9a0be93d68a7a43920fa723b06ac617
aliemre2023 <aliemrekaya239@gmail.com>
2025-05-25 01:38:52 +0300

    a test

---
+++ code/test_gen.py
@@ -0,0 +1,25 @@
+def create_test_file():
+    """
+    Creates a Python test file with a simple testing function
+    """
+    test_content = '''
+def testing():
+    """
+    A simple test function that does nothing and just passes
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("Running testing function...")
+    testing()
+    print("Testing complete!")
+'''
+    
+    with open("code/tester.py", "w") as f:
+        f.write(test_content)
+    
+    print("Test file created successfully at code/tester.py")
+
+if __name__ == "__main__":
+    create_test_file()

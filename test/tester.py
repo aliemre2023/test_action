9669c6922eb585695085b6c2c37dde8ed320719e
aliemre2023 <93014021+aliemre2023@users.noreply.github.com>
2025-05-24 22:41:21 +0000

    Add test file

---
+++ test/tester.py
@@ -0,0 +1,12 @@
+
+def testing():
+    """
+    A simple test function that does nothing and just passes
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("Running testing function...")
+    testing()
+    print("Testing complete!")
